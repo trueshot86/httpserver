a988b0032a0f0d8affbf8156077a63c7379816c5
sugihara <trueshot86@gmail.com>
2020-10-31 22:41:58 +0900

    first

---
+++ server_p2.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import urlparse
+import SimpleHTTPServer
+import BaseHTTPServer
+
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        url = urlparse.urlparse(self.path)
+        request_file_path = url.path.strip('/')
+
+        if not os.path.exists(request_file_path):
+            self.path = 'index.html'
+
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
+host = '0.0.0.0'
+try:
+    port = int(sys.argv[1])
+except IndexError:
+    port = 8000
+httpd = BaseHTTPServer.HTTPServer((host, port), Handler)
+
+print 'Serving HTTP on %s port %d ...' % (host, port)
+httpd.serve_forever()
